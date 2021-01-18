c2fd2d55b8513951a64d99eee0793fb3d20e58ea
jkl <189156395@qq.com>
2021-01-18 22:11:11 +0800

    格式修改

--- src/api/edu/video.js
+++ src/api/edu/video.js
@@ -1,6 +1,8 @@
 import request from '@/utils/request'
+
 export default {
-  //添加章节
+
+
   addVideo(video) {
     return request({
       url: '/eduservice/video/addVideo',
@@ -8,14 +10,16 @@ export default {
       data: video
     })
   },
-  //根据id查询章节
+
+
   getVideo(videoId) {
     return request({
       url: `/eduservice/video/getVideoInfo/${videoId}`,
       method: 'get'
     })
   },
-  //修改章节
+
+
   updateVideo(video) {
     return request({
       url: '/eduservice/video/updateVideo',
@@ -23,10 +27,11 @@ export default {
       data: video
     })
   },
-  //删除章节
+
+
   deleteVideo(videoId) {
     return request({
-      url: '/eduservice/video/'+videoId,
+      url: '/eduservice/video/' + videoId,
       method: 'delete'
     })
   },
