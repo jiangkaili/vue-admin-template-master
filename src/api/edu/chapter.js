39b652ac9bf0e4cccbced5cc244dff100a47febc
jkl <189156395@qq.com>
2021-01-22 09:45:33 +0800

    格式调整

--- src/api/edu/chapter.js
+++ src/api/edu/chapter.js
@@ -1,13 +1,15 @@
 import request from '@/utils/request'
 export default {
-  //1 根据课程id获取章节和小节数据列表
+
+
   getAllChapterVideo(courseId) {
     return request({
       url: '/eduservice/chapter/getChapterVideo/'+courseId,
       method: 'get'
     })
   },
-  //添加章节
+
+
   addChapter(chapter) {
     return request({
       url: '/eduservice/chapter/addChapter',
@@ -15,14 +17,16 @@ export default {
       data: chapter
     })
   },
-  //根据id查询章节
+
+
   getChapter(chapterId) {
     return request({
       url: '/eduservice/chapter/getChapterInfo/'+chapterId,
       method: 'get'
     })
   },
-  //修改章节
+
+
   updateChapter(chapter) {
     return request({
       url: '/eduservice/chapter/updateChapter',
@@ -30,7 +34,8 @@ export default {
       data: chapter
     })
   },
-  //删除章节
+
+
   deleteChapter(chapterId) {
     return request({
       url: '/eduservice/chapter/'+chapterId,
