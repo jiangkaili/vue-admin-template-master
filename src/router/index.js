e1797a2be6d4557480c35f79d3718ee40a7841de
jkl <189156395@qq.com>
2021-02-18 14:20:33 +0800

    统计相关

--- src/router/index.js
+++ src/router/index.js
@@ -134,6 +134,30 @@ export const constantRouterMap = [
   },
 
 
+
+  {
+    path: '/sta',
+    component: Layout,
+    redirect: '/sta/table',
+    name: '统计分析',
+    meta: {title: '统计分析', icon: 'example'},
+    children: [
+      {
+        path: 'create',
+        name: '生成数据',
+        component: () => import('@/views/sta/create'),
+        meta: {title: '生成数据', icon: 'table'}
+      },
+      {
+        path: 'show',
+        name: '图标显示',
+        component: () => import('@/views/sta/show'),
+        meta: {title: '图标显示', icon: 'tree'}
+      }
+    ]
+  },
+
+
   {
     path: '/example',
     component: Layout,
